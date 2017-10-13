2951e719e2a28f116f174822f996747f438f05cb
admhny <adam@adamhaney.co>
2017-10-13 11:21:12 -0700

    Updated Mobile
    Gallery Buttons

--- _includes/js/gallery.js
+++ _includes/js/gallery.js
@@ -65,7 +65,7 @@ $('.js-light-box-close').click(function(e){
 $('.js-more-info').click(function(){
 
   $(this).text(function(i, text){
-    return text === "Show info" ? "Hide info" : "Hide info";
+    return text === "Show info" ? "Hide info" : "Show info";
   });
   $('.js-light-box-txt').toggleClass('is-displayed');
 });
