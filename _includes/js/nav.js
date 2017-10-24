99dee2a7354718cda94d4ca5db40f0a145ce23a6
admhny <adam@adamhaney.co>
2017-10-24 13:28:22 -0700

    Swipe up to close on nav

--- _includes/js/nav.js
+++ _includes/js/nav.js
@@ -1,3 +1,20 @@
 $('.js-nav-trigger, .adam-haney-logo').click(function() {
   $('.nav, .js-nav-trigger').toggleClass('is-triggered');
 });
+
+function closeNav() {
+  $('.nav')
+    .fadeOut(500)
+    .parent('.nav')
+      .removeClass('is-triggered');
+  $('.js-light-box-arrows').removeClass('is-visible');
+  $('html, body').removeAttr('style')
+    .unbind('touchmove, pinchZoom');
+  return false;
+}
+$('.nav').swipe({ // user swipes on slide
+  swipeUp:function() {
+    closeNav();
+  },
+  threshold:68 // min swipe length of 68px
+});
