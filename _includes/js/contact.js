9cc568fc8b6f8b2d06bfb314ec1a4f21de321ffa
admhny <adam@adamhaney.co>
2019-06-12 15:12:38 -0700

    Added Motion Page

--- _includes/js/contact.js
+++ _includes/js/contact.js
@@ -1,18 +1,20 @@
 var $contactForm = $('.contact-form');
-$contactForm.validate({
-  submitHandler: function(form) {
-    $.ajax({
-      url: '//formspree.io/adam@adamhaney.co',
-      method: 'POST',
-      data: $contactForm.serialize(),
-      dataType: "json",
-      success: function(data) {
-        console.log('success');
-        $('.submit-success').fadeIn(300);
-      }
-    });
-  }
-});
+if ($contactForm.length) {
+  $contactForm.validate({
+    submitHandler: function(form) {
+      $.ajax({
+        url: '//formspree.io/adam@adamhaney.co',
+        method: 'POST',
+        data: $contactForm.serialize(),
+        dataType: "json",
+        success: function(data) {
+          console.log('success');
+          $('.submit-success').fadeIn(300);
+        }
+      });
+    }
+  });
+}
 $('.submit-close').click(function() {
   $('.submit-success').fadeOut(300);
 });
